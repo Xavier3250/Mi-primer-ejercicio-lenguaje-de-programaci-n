e698b49bf579f76e6063e9268278ec77fdf775d0
Xavier3250 <hurtadox71@gmail.com>
2026-04-30 11:31:03 -0400

    Add files via upload

--- ejercicio de aprendizaje 3 For y If.cpp
+++ ejercicio de aprendizaje 3 For y If.cpp
@@ -1,31 +1,36 @@
 #include <iostream>
 using namespace std;
 int main(){
-	int pares=0;
-	int impar=0;
-	int numero=0;
-	int sumapares=0;
-	int sumaimpares=0;
-	
-	for(int i=1 ; i<=10 ; i++){
-		cout<<"ingrese numero"<<endl;
-		cin>>numero;
-		
-		if(numero % 2 == 0){
-			cout<<"El numero es par"<<endl;
-			pares+=1;
-			sumapares+= numero;
-		}else{
-			cout<<"el numero impar"<<endl;
-		      impar+=1;
-		      sumaimpares+= numero;
-		} 
-		    
-	}
-	cout<<"la cantidad de suma de los pares: "<<sumapares<<endl;
-		cout<<"la cantidad de suma de los impares: "<<sumaimpares<<endl;
-	cout<<"la cantidad de numeros impares son: "<<impar<<endl;
-	cout<<"la cantidad de numeros pares son: "<<pares<<endl;
+ int barrilesA=0;
+ int barrilesB=0;
+ int pureza=0;
+ int sumabarrilesA=0;
+ int sumabarrilesB=0;
+ int PromediodebarrilesA=0;
+ int PromediodebarrilesB=0;
+  for(int i=1 ; i<=8 ; i++){
+  	cout<<"ingrese nivel de pureza del 1 al 100"<<endl;
+  	cin>>pureza;
+  	if(pureza>=70 && pureza<=100){
+  		cout<<"ES grado A"<<endl;
+  		sumabarrilesA+=pureza;
+  		barrilesA+=1;
+	  }else if(pureza<70 && pureza >=1){
+	  	cout<<"Es grado B"<<endl;
+	  	sumabarrilesB+=pureza;
+	  	barrilesB+=1;
+	  }else{
+	  	cout<<"Error: Pureza invalida" <<endl;
+	  }
+  }
+  PromediodebarrilesA= sumabarrilesA / barrilesA;
+  PromediodebarrilesB= sumabarrilesB / barrilesB;
+  cout<<"Suma barriles de pureza A: "<<sumabarrilesA<<endl;
+  cout<<"Suma barriles de pureza B: "<<sumabarrilesB<<endl;
+  cout<<"Suma barriles de A: "<<barrilesA<<endl;
+  cout<<"Suma barriles de B: "<<barrilesB<<endl;
+  cout<<"Promedio de pureza de barriles grado A: "<<PromediodebarrilesA<<endl;
+  cout<<"Promedio de pureza de barriles grado B: "<<PromediodebarrilesB<<endl;
 	
 	return 0;
 }
